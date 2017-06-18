d136e32214e954aeb583b0818d2cfa94a61d2b14
Allan Bastian <allan.batian9.ab@gmail.com>
2017-06-19 02:23:38 +0530

    Switch set

--- app/src/main/java/com/adgvit/work/MainActivity.java
+++ app/src/main/java/com/adgvit/work/MainActivity.java
@@ -2,12 +2,38 @@ package com.adgvit.work;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+    private ToggleButton toggleButton;
+    private Switch aSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        aSwitch = (Switch) findViewById(R.id.switch1);
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked == true) {
+                    //show Pending work
+                    aSwitch.setText("Pending");
+                    showPending();
+                } else {
+                    //show Completed work
+                    aSwitch.setText("Completed");
+                    showCompleted();
+                }
+            }
+        });
+    }
+
+    private void showCompleted() {
+    }
+
+    private void showPending() {
     }
 }
